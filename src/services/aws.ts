9363b016c75d83fe0ddaaff9b33775f49081b3f3
Igor Reis <63017440+eakeur@users.noreply.github.com>
2024-08-12 10:11:03 -0300

    fix fix creds

--- src/services/aws.ts
+++ src/services/aws.ts
@@ -1,8 +1,8 @@
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-  accessKeyId: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-  secretAccessKey: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+  accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
   region: 'us-west-1',
 });
 
